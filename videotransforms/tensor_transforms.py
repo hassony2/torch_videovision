f96149c569b1a5f423432951789750d96a9b98a5
Yana Hasson <yanahasson@gmail.com>
2018-02-14 17:18:11 +0100

    Add SpatialRandomCrop that crops numpy array or tensor in one slicing operation

--- videotransforms/tensor_transforms.py
+++ videotransforms/tensor_transforms.py
@@ -1,3 +1,5 @@
+import random
+
 from videotransforms.utils import functional as F
 
 
@@ -26,3 +28,32 @@ class Normalize(object):
             Tensor: Normalized stack of image of image
         """
         return F.normalize(tensor, self.mean, self.std)
+
+
+class SpatialRandomCrop(object):
+    """Crops a random spatial crop in a spatio-temporal
+    numpy or tensor input [Channel, Time, Height, Width]
+    """
+
+    def __init__(self, size):
+        """
+        Args:
+            size (tuple): in format (height, width)
+        """
+        self.size = size
+
+    def __call__(self, tensor):
+        h, w = self.size
+        _, _, tensor_h, tensor_w = tensor.shape
+
+        if w > tensor_w or h > tensor_h:
+            error_msg = (
+                'Initial tensor spatial size should be larger then '
+                'cropped size but got cropped sizes : ({w}, {h}) while '
+                'initial tensor is ({t_w}, {t_h})'.format(
+                    t_w=tensor_w, t_h=tensor_h, w=w, h=h))
+            raise ValueError(error_msg)
+        x1 = random.randint(0, tensor_w - w)
+        y1 = random.randint(0, tensor_h - h)
+        cropped = tensor[:, :, y1:y1 + h, x1:x1 + h]
+        return cropped
