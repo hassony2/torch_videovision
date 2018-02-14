8e82448abf16d2c400da25bb19e6c8b01c433a0c
Yana Hasson <yanahasson@gmail.com>
2018-02-14 17:23:37 +0100

    Scale now admits size as scale with same logic as torchvision
    If int is provided, smallest dimension of image is resized to given shape
    while preserving ratio

--- videotransforms/video_transforms.py
+++ videotransforms/video_transforms.py
@@ -1,5 +1,8 @@
 import cv2
+import numbers
 import random
+
+from matplotlib import pyplot as plt
 import numpy as np
 import PIL
 import torch
@@ -10,8 +13,8 @@ class Compose(object):
     """Composes several transforms
 
     Args:
-        transforms (list of ``Transform`` objects): list of transforms
-        to compose
+    transforms (list of ``Transform`` objects): list of transforms
+    to compose
     """
 
     def __init__(self, transforms):
@@ -31,11 +34,11 @@ class RandomHorizontalFlip(object):
     def __call__(self, clip):
         """
         Args:
-            img (PIL.Image or numpy.ndarray): List of images to be cropped
-                in format (h, w, c) in numpy.ndarray
+        img (PIL.Image or numpy.ndarray): List of images to be cropped
+        in format (h, w, c) in numpy.ndarray
 
         Returns:
-            PIL.Image or numpy.ndarray: Randomly flipped clip
+        PIL.Image or numpy.ndarray: Randomly flipped clip
         """
         if random.random() < 0.5:
             if isinstance(clip[0], np.ndarray):
@@ -57,58 +60,90 @@ class Scale(object):
     interpolate
 
     Args:
-        interpolation (str): Can be one of 'nearest', 'bilinear'
-            defaults to nearest
+    interpolation (str): Can be one of 'nearest', 'bilinear'
+    defaults to nearest
+    size (tuple): (widht, height)
     """
 
     def __init__(self, size, interpolation='nearest'):
-        if type(size) is int:
-            self.size = (size, size)
-        else:
-            self.size = size
+        self.size = size
         self.interpolation = interpolation
 
     def __call__(self, clip):
         if isinstance(clip[0], np.ndarray):
+            if isinstance(self.size, numbers.Number):
+                im_h, im_w, im_c = clip[0].shape
+                # Min spatial dim already matches minimal size
+                if (im_w <= im_h
+                        and im_w == self.size) or (im_h <= im_w
+                                                   and im_h == self.size):
+                    return clip
+                new_h, new_w = get_resize_sizes(im_h, im_w, self.size)
+                size = (new_w, new_h)
+            else:
+                size = self.size[1], self.size[0]
             if self.interpolation == 'bilinear':
                 np_inter = cv2.INTER_LINEAR
             else:
                 np_inter = cv2.INTER_NEAREST
             scaled = [
-                cv2.resize(img, self.size, interpolation=np_inter)
-                for img in clip
+                cv2.resize(img, size, interpolation=np_inter) for img in clip
             ]
         elif isinstance(clip[0], PIL.Image.Image):
+            if isinstance(self.size, numbers.Number):
+                im_w, im_h = clip[0].size
+                # Min spatial dim already matches minimal size
+                if (im_w <= im_h
+                        and im_w == self.size) or (im_h <= im_w
+                                                   and im_h == self.size):
+                    return clip
+                new_h, new_w = get_resize_sizes(im_h, im_w, self.size)
+                size = (new_w, new_h)
+            else:
+                size = self.size[1], self.size[0]
             if self.interpolation == 'bilinear':
                 pil_inter = PIL.Image.NEAREST
             else:
                 pil_inter = PIL.Image.BILINEAR
-            scaled = [img.resize(self.size, pil_inter) for img in clip]
+            scaled = [img.resize(size, pil_inter) for img in clip]
         else:
             raise TypeError('Expected numpy.ndarray or PIL.Image' +
                             'but got list of {0}'.format(type(clip[0])))
         return scaled
 
 
+def get_resize_sizes(im_h, im_w, size):
+    if im_w < im_h:
+        ow = size
+        oh = int(size * im_h / im_w)
+    else:
+        oh = size
+        ow = int(size * im_w / im_h)
+    return oh, ow
+
+
 class RandomCrop(object):
     """Extract random crop at the same location for a list of images
 
     Args:
-        size (sequence or int): Desired output size for the
-            crop in format (h, w)
+    size (sequence or int): Desired output size for the
+    crop in format (h, w)
     """
 
     def __init__(self, size):
+        if isinstance(self.size, numbers.Number):
+            size = (size, size)
+
         self.size = size
 
     def __call__(self, clip):
         """
         Args:
-            img (PIL.Image or numpy.ndarray): List of images to be cropped
-                in format (h, w, c) in numpy.ndarray
+        img (PIL.Image or numpy.ndarray): List of images to be cropped
+        in format (h, w, c) in numpy.ndarray
 
         Returns:
-            PIL.Image or numpy.ndarray: Cropped list of images
+        PIL.Image or numpy.ndarray: Cropped list of images
         """
         h, w = self.size
         if isinstance(clip[0], np.ndarray):
@@ -128,11 +163,66 @@ class RandomCrop(object):
 
         x1 = random.randint(0, im_w - w)
         y1 = random.randint(0, im_h - h)
+        cropped = crop_clip(clip, y1, x1, h, w)
+
+        return cropped
+
+
+class CenterCrop(object):
+    """Extract center crop at the same location for a list of images
+
+    Args:
+    size (sequence or int): Desired output size for the
+    crop in format (h, w)
+    """
+
+    def __init__(self, size):
+        if isinstance(size, numbers.Number):
+            size = (size, size)
+
+        self.size = size
+
+    def __call__(self, clip):
+        """
+        Args:
+        img (PIL.Image or numpy.ndarray): List of images to be cropped
+        in format (h, w, c) in numpy.ndarray
+
+        Returns:
+        PIL.Image or numpy.ndarray: Cropped list of images
+        """
+        h, w = self.size
         if isinstance(clip[0], np.ndarray):
-            cropped = [img[y1:y1 + h, x1:x1 + w, :] for img in clip]
+            im_h, im_w, im_c = clip[0].shape
         elif isinstance(clip[0], PIL.Image.Image):
-            cropped = [img.crop((x1, y1, x1 + w, y1 + h)) for img in clip]
+            im_w, im_h = clip[0].size
         else:
             raise TypeError('Expected numpy.ndarray or PIL.Image' +
                             'but got list of {0}'.format(type(clip[0])))
+        if w > im_w or h > im_h:
+            error_msg = (
+                'Initial image size should be larger then '
+                'cropped size but got cropped sizes : ({w}, {h}) while '
+                'initial image is ({im_w}, {im_h})'.format(
+                    im_w=im_w, im_h=im_h, w=w, h=h))
+            raise ValueError(error_msg)
+
+        x1 = int(round((im_w - w) / 2.))
+        y1 = int(round((im_h - h) / 2.))
+        cropped = crop_clip(clip, y1, x1, h, w)
+
         return cropped
+
+
+def crop_clip(clip, min_h, min_w, h, w):
+    if isinstance(clip[0], np.ndarray):
+        cropped = [img[min_h:min_h + h, min_w:min_w + w, :] for img in clip]
+
+    elif isinstance(clip[0], PIL.Image.Image):
+        cropped = [
+            img.crop((min_w, min_h, min_w + w, min_h + h)) for img in clip
+        ]
+    else:
+        raise TypeError('Expected numpy.ndarray or PIL.Image' +
+                        'but got list of {0}'.format(type(clip[0])))
+    return cropped
