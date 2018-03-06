459eeab6b81b515bfcd9109536c285c7c5726d6e
Yana Hasson <yana.hasson@inria.fr>
2018-03-06 20:28:45 +0100

    Add README and update test demo script

---
+++ README.md
@@ -0,0 +1,29 @@
+torch_videovision - Basic Video transforms for Pytorch
+======================================================
+
+
+This repository implements several basic data-augmentation transforms for pytorch video inputs 
+
+![transform_image](data/transform_cat.png)
+
+The idea was to produce the equivalent of [torchvision transforms](https://github.com/pytorch/vision/tree/master/torchvision/transforms) for video inputs. (The code is therefore widely based on the code from this repository :) ) 
+
+The basic paradigm is that dataloading should produce videoclips as a **list of PIL Images or numpy.ndarrays** (in format as read by opencv).
+
+Several transforms are then provided in [video_transforms](videotransforms/video_transforms.py) that expect such inputs.
+Each transform iterates on all the images in the list and applies the wanted augmentation.
+
+We then have to convert those inputs to torch tensors.
+This can be produced by the [volume_transform](videotransforms/volume_transforms.py).**ClipToTensor** class, which produces a video volume in format (n_channels, n_images, height, width) where n_channels = 3 in case of images.
+
+When randomness is involved, the same random parameters (crop size, scale size, rotation angle,...) are applied to all the frames.
+
+Transforms can be composed just as in torchvision with video_transforms.Compose.
+
+To quickly see a demo of the transformations, run
+
+`python testtransforms.py`
+
+This should produce something like the following image top image (this is a dummy clip for now, so the same image is repeated several times)
+
+On the first row you have the original clip (visualized frame by frame), and on the second row, the transformed one.
--- testransforms.py
+++ testransforms.py
@@ -4,22 +4,28 @@ from matplotlib import pyplot as plt
 import numpy as np
 from PIL import Image
 
-from videotransforms.video_transforms import Compose, Scale, RandomCrop
-from videotransforms.volume_transforms import ToTensor
+from videotransforms.video_transforms import Compose, Resize, RandomCrop, RandomRotation
+from videotransforms.volume_transforms import ClipToTensor
 
 img_path = 'data/cat/cat1.jpeg'
 
+# Set transform parameters
 scale_size = (256, 256)
 crop_size = (200, 200)
+max_rotation_angle = 30
 channel_nb = 3
 clip_size = 5
 
+# Initialize transforms
 video_transform_list = [
-    Scale(scale_size),
+    RandomRotation(max_rotation_angle),
+    Resize(scale_size),
     RandomCrop(crop_size),
-    ToTensor(channel_nb=channel_nb)
+    ClipToTensor(channel_nb=channel_nb)
 ]
 video_transform = Compose(video_transform_list)
+
+# Create dummy video clip
 img = Image.open(img_path)
 clip = [img] * clip_size
 tensor_clip = video_transform(clip)
@@ -35,8 +41,15 @@ spatial_dim = tuple(tensor_shape[2:4])
 assert spatial_dim == crop_size, 'Final tensor spatial dims {} should be crop_size {}'.format(
     spatial_dim, clip_size)
 
-recover_img = tensor_clip.numpy()[:, 2, :, :].transpose(1, 2, 0)
-plt.imshow(recover_img)
+# Display resulting image
+fig, axes = plt.subplots(2, clip_size)
+for img_idx in range(clip_size):
+    img = clip[img_idx]
+    axes[0, img_idx].imshow(img)
+    axes[0, img_idx].axis('off')
+    axes[0, 0].set_title('original clip')
+    recover_img = tensor_clip.numpy()[:, img_idx, :, :].transpose(1, 2, 0)
+    axes[1, img_idx].imshow(recover_img)
+    axes[1, img_idx].axis('off')
+    axes[1, 0].set_title('transformed clip')
 plt.show()
-import pdb
-pdb.set_trace()
