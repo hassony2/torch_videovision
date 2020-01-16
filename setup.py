803822154a067535176d66a5aa594e73e9d4ff50
Avi Aminov <aviaminov@gmail.com>
2020-01-16 23:57:02 +0200

    leftover fix

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/hassony2/torch_videovision",
-    packages=["torchvideotransforms"],
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
