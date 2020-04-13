5f94c67578222c019b535cf05dc8f13078a4c027
iory <ab.ioryz@gmail.com>
2020-04-14 04:03:29 +0900

    Add probability to RandomHorizontalFlip

--- torchvideotransforms/video_transforms.py
+++ torchvideotransforms/video_transforms.py
@@ -26,9 +26,14 @@ class Compose(object):
 
 
 class RandomHorizontalFlip(object):
-    """Horizontally flip the list of given images randomly
-    with a probability 0.5
+    """Horizontally flip the list of given images randomly with a given probability.
+
+    Args:
+        p (float): probability of the image being flipped. Default value is 0.5
     """
+
+    def __init__(self, p=0.5):
+        self.p = p
 
     def __call__(self, clip):
         """
@@ -39,7 +44,7 @@ class RandomHorizontalFlip(object):
         Returns:
         PIL.Image or numpy.ndarray: Randomly flipped clip
         """
-        if random.random() < 0.5:
+        if random.random() < self.p:
             if isinstance(clip[0], np.ndarray):
                 return [np.fliplr(img) for img in clip]
             elif isinstance(clip[0], PIL.Image.Image):
@@ -50,6 +55,9 @@ class RandomHorizontalFlip(object):
                 raise TypeError('Expected numpy.ndarray or PIL.Image' +
                                 ' but got list of {0}'.format(type(clip[0])))
         return clip
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(p={})'.format(self.p)
 
 
 class RandomResize(object):
