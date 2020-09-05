8a5002c74794d5c34af58046748addab15a47eb3
kiwi_fung <fjchange@hotmail.com>
2020-09-05 11:13:12 +0800

    Update video_transforms.py

--- torchvideotransforms/video_transforms.py
+++ torchvideotransforms/video_transforms.py
@@ -2,7 +2,7 @@ import numbers
 import random
 import numpy as np
 import PIL
-import skimage
+import skimage.transform
 import torchvision
 
 from . import functional as F
@@ -195,7 +195,101 @@ class RandomCrop(object):
 
         return cropped
 
+class RandomResizedCrop(object):
+    """Crop the given list of PIL Images to random size and aspect ratio.
 
+    A crop of random size (default: of 0.08 to 1.0) of the original size and a random
+    aspect ratio (default: of 3/4 to 4/3) of the original aspect ratio is made. This crop
+    is finally resized to given size.
+    This is popularly used to train the Inception networks.
+
+    Args:
+        size: expected output size of each edge
+        scale: range of size of the origin size cropped
+        ratio: range of aspect ratio of the origin aspect ratio cropped
+        interpolation: Default: PIL.Image.BILINEAR
+    """
+
+    def __init__(self, size, scale=(0.08, 1.0), ratio=(3. / 4., 4. / 3.), interpolation=Image.BILINEAR):
+        if isinstance(size, (tuple, list)):
+            self.size = size
+        else:
+            self.size = (size, size)
+        if (scale[0] > scale[1]) or (ratio[0] > ratio[1]):
+            warnings.warn("range should be of kind (min, max)")
+
+        self.interpolation = interpolation
+        self.scale = scale
+        self.ratio = ratio
+
+    @staticmethod
+    def get_params(img, scale, ratio):
+        """Get parameters for ``crop`` for a random sized crop.
+
+        Args:
+            img (list of PIL Image): Image to be cropped.
+            scale (tuple): range of size of the origin size cropped
+            ratio (tuple): range of aspect ratio of the origin aspect ratio cropped
+
+        Returns:
+            tuple: params (i, j, h, w) to be passed to ``crop`` for a random
+                sized crop.
+        """
+        if isinstance(clip[0], np.ndarray):
+            height, width, im_c = clip[0].shape
+        elif isinstance(clip[0], PIL.Image.Image):
+            width, height = clip[0].size
+        area = height * width
+
+        for _ in range(10):
+            target_area = random.uniform(*scale) * area
+            log_ratio = (math.log(ratio[0]), math.log(ratio[1]))
+            aspect_ratio = math.exp(random.uniform(*log_ratio))
+
+            w = int(round(math.sqrt(target_area * aspect_ratio)))
+            h = int(round(math.sqrt(target_area / aspect_ratio)))
+
+            if 0 < w <= width and 0 < h <= height:
+                i = random.randint(0, height - h)
+                j = random.randint(0, width - w)
+                return i, j, h, w
+
+        # Fallback to central crop
+        in_ratio = float(width) / float(height)
+        if (in_ratio < min(ratio)):
+            w = width
+            h = int(round(w / min(ratio)))
+        elif (in_ratio > max(ratio)):
+            h = height
+            w = int(round(h * max(ratio)))
+        else:  # whole image
+            w = width
+            h = height
+        i = (height - h) // 2
+        j = (width - w) // 2
+        return i, j, h, w
+
+    def __call__(self, imgs):
+        """
+        Args:
+            list of img (PIL Image): Image to be cropped and resized.
+
+        Returns:
+            list of PIL Image: Randomly cropped and resized image.
+        """
+        i, j, h, w = self.get_params(imgs, self.scale, self.ratio)
+        imgs=F.crop_clip(imgs,i,j,h,w)
+        return F.resize_clip(imgs,self.size,self.interpolation)
+        # return F.resized_crop(img, i, j, h, w, self.size, self.interpolation)
+
+    def __repr__(self):
+        interpolate_str = _pil_interpolation_to_str[self.interpolation]
+        format_string = self.__class__.__name__ + '(size={0}'.format(self.size)
+        format_string += ', scale={0}'.format(tuple(round(s, 4) for s in self.scale))
+        format_string += ', ratio={0}'.format(tuple(round(r, 4) for r in self.ratio))
+        format_string += ', interpolation={0})'.format(interpolate_str)
+        return format_string
+    
 class RandomRotation(object):
     """Rotate entire clip randomly by a random angle within
     given bounds
